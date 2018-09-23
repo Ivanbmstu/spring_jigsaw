db5e50d47ad284d36e9adb821cce7fa3c514da7e
Ivan Pohodnya <ipokhodnya@alfabank.ru>
2018-09-23 21:52:47 +0300

    no module run

--- client/jar-runner.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm ./build/lib/mp/javax.activation-api-1.2.0.jar
-java -p "./build/libs:./build/lib/mp" -cp "./build/lib/cp/*" "$@" \
---patch-module java.annotation=./build/lib/pm/jsr305-3.0.1.jar \
---add-opens java.base/java.lang=spring.core \
--m client.main/com.example.front.FrontApplication
--- runner.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# увы и ах jar запустить с модулями не удастся см jar --describe-module --file=./build/libs/demo-0.0.1-SNAPSHOT.jar 
-# удастся запустить просто проект, но не с модулем из module-info 
-java -p ./build/libs/demo-0.0.1-SNAPSHOT.jar --add-modules java.sql --show-module-resolution -m demo
