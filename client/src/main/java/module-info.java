278a03f277958364146b9dbf729e23840b7db611
Ivan Pohodnya <ipokhodnya@alfabank.ru>
2018-09-23 23:00:57 +0300

    fix run tests

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,11 +1,11 @@
-module client.main {
+open module client.main {
     requires static lombok; // static - required at compile, do not required at runtime
     
     requires spring.context;
     requires java.xml.bind; // jaxb api
     requires com.sun.xml.bind; // jaxb runtime impl
     
-    requires slf4j.api;
+//    requires slf4j.api;
 
     requires java.desktop; // required by lombok...
 }
