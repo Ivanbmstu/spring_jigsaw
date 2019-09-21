c39bb48734443a6ea2b1cab6d025c626c0ac0db9
Ivan Pohodnya <pohodnyaivan@gmail.com>
2019-09-22 00:47:19 +0300

    init

--- client/check-module.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-jar --describe-module --file=./build/libs/client-0.0.1-SNAPSHOT.jar
--- client/runner.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-java -p "./build/classes/java/main/:./build/lib/mp" -cp "./build/lib/cp/*" "$@" \
---add-opens java.base/java.util=xstream \
---add-opens java.base/java.lang.reflect=xstream \
---add-opens java.base/java.text=xstream \
---add-opens java.desktop/java.awt.font=xstream \
---add-reads front.module=ALL-UNNAMED \
--m front.module/com.example.front.FrontApplication
